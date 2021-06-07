c9ffa1fefddf8c36711c1bfa90bfef1d6c5135da
Frank Schiebel <frank@talheim.net>
2021-06-07 18:35:03 +0200

    Kommentierte Roh-Version

--- syntax.php
+++ syntax.php
@@ -18,7 +18,7 @@ class syntax_plugin_projekt extends DokuWiki_Syntax_Plugin
      */
     public function getType()
     {
-        return 'FIXME: container|baseonly|formatting|substition|protected|disabled|paragraphs';
+        return 'substition';
     }
 
     /**
@@ -26,7 +26,7 @@ class syntax_plugin_projekt extends DokuWiki_Syntax_Plugin
      */
     public function getPType()
     {
-        return 'FIXME: normal|block|stack';
+        return 'block';
     }
 
     /**
@@ -34,7 +34,7 @@ class syntax_plugin_projekt extends DokuWiki_Syntax_Plugin
      */
     public function getSort()
     {
-        return FIXME;
+        return 222;
     }
 
     /**
@@ -43,15 +43,12 @@ class syntax_plugin_projekt extends DokuWiki_Syntax_Plugin
      * @param string $mode Parser mode
      */
     public function connectTo($mode)
-    {
-        $this->Lexer->addSpecialPattern('<FIXME>', $mode, 'plugin_projekt');
-//        $this->Lexer->addEntryPattern('<FIXME>', $mode, 'plugin_projekt');
+    {  
+        // Wenn das Pattern gematcht wird springt das Plugin zur Methode "handle" 
+        // ("Handle Match"), dabei wird er gematchte String als $match übergeben und kann weiter 
+        // verarbeitet werden - s.u. 
+        $this->Lexer->addSpecialPattern('\{\{projekt>.+?\}\}', $mode, 'plugin_projekt');
     }
-
-//    public function postConnect()
-//    {
-//        $this->Lexer->addExitPattern('</FIXME>', 'plugin_projekt');
-//    }
 
     /**
      * Handle matches of the projekt syntax
@@ -65,9 +62,21 @@ class syntax_plugin_projekt extends DokuWiki_Syntax_Plugin
      */
     public function handle($match, $state, $pos, Doku_Handler $handler)
     {
-        $data = array();
+        // Geschweifte Klammern entfernen
+        $match = substr($match, 2, -2);
+        // Splitten am ">". Erster Teil geht nach "command"
+        list($command, $match) = explode('>', $match, 2);
+        
+        // Weitere Optionen können übergeben werden, wenn Bedarf ist
+        // list($input, $options) = split('#', $match, 2);
 
-        return $data;
+        // Wir geben als "options" jetzt einfach mal den Teil nach dem ">"
+        // an die render Methode weiter:
+        $options = $match;
+
+        // Das Array, das hier zurückgegeben wird, landet als $data 
+        // in der render-Methode, s.u. 
+        return array($command, $options);
     }
 
     /**
@@ -81,10 +90,28 @@ class syntax_plugin_projekt extends DokuWiki_Syntax_Plugin
      */
     public function render($mode, Doku_Renderer $renderer, $data)
     {
+        // Wir rendern nur HTML, sonst nix!
         if ($mode !== 'xhtml') {
             return false;
         }
 
+        // Die handle Methode liefert in $data ein Array mit 2 Elementen (s.o.)
+        // das klamüsern wir jetzt mal auseinander:
+        $command = $data[0];
+        $options = $data[1];
+
+
+        // Alles was mit dem Verkettungs-Operator "."
+        // an $renderer->doc "angehängt" wird, 
+        // wird als Ersetzung des gematche Patterns in der 
+        // Wiki-Seite eingefügt. Der Browser will HTML sehen.
+        $renderer->doc .= "<h2>Diese Überschrift kommt aus dem Plugin projekt</h2>";
+        $renderer->doc .= "<ul>";
+        $renderer->doc .= "<li>Als <tt>command</tt> wurde " . $command . " übergeben.</li>";
+        $renderer->doc .= "<li>Als <tt>options</tt> wurde " . $options . " übergeben.</li>";
+        $renderer->doc .= "</ul>";
+
+        // Alles gut: 
         return true;
     }
 }
